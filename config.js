5781623de2aaccbc2265bb014e444372013689ff
Hassan <hassanila97@users.noreply.github.com>
2023-09-24 22:22:07 +0200

    add HEADLESS option and update LOGIN_URL

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  ROUTER_LOGIN_URL: 'http://192.168.0.1',
-  WIFI_CONFIG_PATH: '/2.0/gui/#/wifidual/2.4GHz/priv/basic',
+  ROUTER_LOGIN_URL: 'http://192.168.0.1/2.0/gui/',
+  WIFI_CONFIG_PATH: '#/wifidual/2.4GHz/priv/basic',
   USERNAME: 'admin',
   PASSWORD: '',
+  HEADLESS: true,
   API_PORT: 3263
 }
