344736aa6294d80c67ab01a501db1406d5672b30
Hassan Ila <15068696+hassanila97@users.noreply.github.com>
2023-12-06 11:02:31 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,16 @@
-npm start
+:start
+title --- wifi router toggle ---
+color 0a
+@echo off
+cls
+node index
+
+if %ERRORLEVEL% == 0 (
+	timeout /t 1800
+	::pause >nul
+	goto start
+) else if %ERRORLEVEL% == 1 (
+	timeout /t 1800
+	::pause >nul
+	goto start
+)
